af28eb81b92396cf44054915ee783ff27a907bd2
Connor Rayburn <connorrayburn.work@gmail.com>
2021-08-10 15:35:31 -0700

    mvp

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,33 @@
-// DO YOUR MAGIC
+const express = require('express');
+const Car = require('./cars-model')
+const router = express.Router()
+const {
+    checkCarId,
+    checkCarPayload,
+    checkVinNumberUnique,
+    checkVinNumberValid,
+} = require('./cars-middleware')
+
+router.get('/', async (req, res, next) => {
+    try{
+        const cars = await Car.getAll()
+        res.json(cars)
+    }catch(err){
+        next(err)
+    }
+})
+
+router.get('/:id', checkCarId, async (req, res, next) => {
+    res.json(req.car)
+})
+
+router.post('/', checkCarPayload, checkVinNumberUnique, checkVinNumberValid, async (req, res, next) => {
+    try{
+        const car = await Car.create(req.body)
+        res.json(car)
+    }catch(err){
+        next(err)
+    }
+})
+
+module.exports = router
